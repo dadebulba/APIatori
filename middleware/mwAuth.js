b935ebe2c0325192021c13afda2c848421a93f93
dadebulba <bdavide17@gmail.com>
2020-02-01 16:14:26 +0100

    error handler fix

--- middleware/mwAuth.js
+++ middleware/mwAuth.js
@@ -2,7 +2,7 @@ const fetch = require("node-fetch");
 const express = require('express');
 const bearerToken = require('express-bearer-token');
 process.env["NODE_CONFIG_DIR"] = "../config/";
-const config = require('config'); 
+const config = require('config');
 const errors = require('../errorMsg.js');
 
 const app = express();
@@ -10,27 +10,31 @@ app.use(bearerToken());
 
 const IdP_URL = config.tokenURL + ":" + config.tokenPort;;
 
-module.exports = async function(req, res, next){
+module.exports = async function (req, res, next) {
 
     let options = {
         method: 'POST',
-        headers: {  'Authorization': `Bearer ${req.token}`,
-                    'action': "verifyToken" 
+        headers: {
+            'Authorization': `Bearer ${req.token}`,
+            'action': "verifyToken"
         }
     }
+    try {
+        let idpResponse = await fetch(IdP_URL, options);
+        if (idpResponse.status == 200) {
+            let body = await idpResponse.json();
 
-    let idpResponse = await fetch(IdP_URL, options);
-    if (idpResponse.status == 200){
-        let body = await idpResponse.json();
-
-        req['uid'] = body.uid;
-        req['gid'] = body.gid;
-        req['role'] = body.role;
-        req['educatorIn'] = body.educatorIn;
-        return next();
-    }
-    else {
-        return res.status(401).json(errors.INVALID_TOKEN);
+            req['uid'] = body.uid;
+            req['gid'] = body.gid;
+            req['role'] = body.role;
+            req['educatorIn'] = body.educatorIn;
+            return next();
+        }
+        else {
+            return res.status(401).json(errors.INVALID_TOKEN);
+        }
+    } catch (err) {
+        next(err)
     }
 
 }
