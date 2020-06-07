f9b6c67bcd545ed5adb52d8088fe06dd498d7667
davide <d.bulbarelli@aliaslab.net>
2020-06-07 19:23:13 +0200

    edited start.sh

--- start.sh
+++ start.sh
@@ -6,20 +6,20 @@ docker run --name mongo --net host -p 27017:27017 -v ~/data:/data/db -d mongo
 
 # Start identity server
 docker build -f microservices/auth/Dockerfile -t dadebulba/apiatori-auth .
-docker run --name auth --net host -p 3000:3000 -e PROD=true -d auth
+docker run --name auth --net host -p 3000:3000 -e PROD=true -d dadebulba/apiatori-auth
 
 # Start gateway
 docker build -f gateway/Dockerfile -t dadebulba/apiatori-gateway .
-docker run --name gateway --net host -p 8080:8080 -e PROD=true -d gateway
+docker run --name gateway --net host -p 8080:8080 -e PROD=true -d dadebulba/apiatori-gateway
 
 # Start spaces service
 docker build -f microservices/spaces/Dockerfile -t dadebulba/apiatori-spaces .
-docker run --name spaces --net host -p 3002:3002 -e PROD=true -d spaces
+docker run --name spaces --net host -p 3002:3002 -e PROD=true -d dadebulba/apiatori-spaces
 
 # Start groups service
 docker build -f microservices/groups/Dockerfile -t dadebulba/apiatori-groups .
-docker run --name groups --net host -p 3003:3003 -e PROD=true -d groups
+docker run --name groups --net host -p 3003:3003 -e PROD=true -d dadebulba/apiatori-groups
 
 # Start users service
 docker build -f microservices/users/Dockerfile -t dadebulba/apiatori-users .
-docker run --name users --net host -p 3004:3004 -e PROD=true -d users
+docker run --name users --net host -p 3004:3004 -e PROD=true -d dadebulba/apiatori-users
