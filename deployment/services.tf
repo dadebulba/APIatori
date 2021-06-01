b046e4409bbd731698cee4a1d65b2e80f2c09701
Pivoz <davide.piva@icloud.com>
2021-06-01 16:36:30 +0200

    Update service.tf

--- deployment/services.tf
+++ deployment/services.tf
@@ -34,3 +34,75 @@ resource "kubernetes_service" "mongodb_service" {
     }
   }
 }
+
+// Auth service
+resource "kubernetes_service" "auth_service" {
+  metadata {
+    name      = "auth"
+    namespace = "default"
+  }
+  spec {
+    selector = {
+      app = "l_auth"
+    }
+    type = "ClusterIP"
+    port {
+      port        = 3000
+      target_port = 3000
+    }
+  }
+}
+
+// Spaces service
+resource "kubernetes_service" "spaces_service" {
+  metadata {
+    name      = "spaces"
+    namespace = "default"
+  }
+  spec {
+    selector = {
+      app = "l_spaces"
+    }
+    type = "ClusterIP"
+    port {
+      port        = 3002
+      target_port = 3002
+    }
+  }
+}
+
+// Groups service
+resource "kubernetes_service" "groups_service" {
+  metadata {
+    name      = "groups"
+    namespace = "default"
+  }
+  spec {
+    selector = {
+      app = "l_groups"
+    }
+    type = "ClusterIP"
+    port {
+      port        = 3003
+      target_port = 3003
+    }
+  }
+}
+
+// Users service
+resource "kubernetes_service" "users_service" {
+  metadata {
+    name      = "users"
+    namespace = "default"
+  }
+  spec {
+    selector = {
+      app = "l_users"
+    }
+    type = "ClusterIP"
+    port {
+      port        = 3004
+      target_port = 3004
+    }
+  }
+}
